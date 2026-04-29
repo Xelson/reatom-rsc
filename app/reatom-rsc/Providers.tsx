91895a6ff9b9dd46cf4ecee8cbb8fcd355828f10
ekke <xelson1337@gmail.com>
2026-04-29 19:41:35 +0300

    fix: move root frame instantiation inside the component

--- app/reatom-rsc/Providers.tsx
+++ app/reatom-rsc/Providers.tsx
@@ -1,13 +1,16 @@
 "use client";
 
-import { PropsWithChildren, Suspense } from "react";
+import { PropsWithChildren, Suspense, useMemo } from "react";
 import { ReatomNextJsContextProvider } from "../reatom-nextjs/Provider";
 import { connectLogger, context } from "@reatom/core";
 
-const rootFrame = context.start();
-rootFrame.run(connectLogger);
-
 export function Providers({ children }: PropsWithChildren) {
+  const rootFrame = useMemo(() => {
+    const frame = context.start();
+    if (typeof window !== "undefined") frame.run(connectLogger);
+    return frame;
+  }, []);
+
   return (
     <Suspense>
       <ReatomNextJsContextProvider frame={rootFrame}>
